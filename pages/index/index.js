c687c53cf83c62a423efd6671fe2b7e5e174ecb2
johnnyzhang <1113638674@qq.com>
2018-03-01 18:06:39 +0800

    little change

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '古诗文小助手',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
