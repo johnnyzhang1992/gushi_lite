0421267c44e438ba4627409cb603d2f37b4358eb
johnnyzhang <1113638674@qq.com>
2018-03-08 00:20:17 +0800

    通过拿取古诗文详情id

---
+++ pages/poem/detail/index.js
@@ -0,0 +1,119 @@
+// pages/poem/detail/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    poem: null,
+    poem_id:null,
+    detail: null,
+    poems_count:0,
+    author:null,
+    content:null,
+    tags:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      poem_id: options.id
+    });
+    wx.showLoading({
+      title: '页面加载中...',
+      mask: true
+    });
+    wx.request({
+      url: 'https://xuegushi.cn/wxxcx/poem/'+options.id,
+      success: res => {
+        if (res.data) {
+          console.log('----------success------------');
+          console.log(res.data);
+          console.log(JSON.parse(res.data.poem.content).content);
+          this.setData({
+            poem: res.data.poem,
+            detail: res.data.detail,
+            poems_count: res.data.poems_count,
+            author:res.data.author,
+            content:JSON.parse(res.data.poem.content),
+            tags: res.data.poem.tags && res.data.poem.tags !='' ? res.data.poem.tags.split(',') : []
+          });
+          wx.hideLoading();
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    wx.hideLoading();
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    wx.showLoading({
+      title: '刷新中...',
+      mask: true
+    });
+    wx.request({
+      url: 'https://xuegushi.cn/wxxcx/poem/'+this.data.poem_id,
+      success: res => {
+        if (res.data) {
+          wx.hideLoading();
+          console.log('----------success------------');
+          console.log(res.data);
+          console.log(JSON.parse(res.data.poem.content).content);
+          this.setData({
+            poem: res.data.poem,
+            detail: res.data.detail,
+            poems_count: res.data.poems_count,
+            author:res.data.author,
+            content:JSON.parse(res.data.poem.content),
+            tags: res.data.poem.tags && res.data.poem.tags !='' ? res.data.poem.tags.split(',') : []
+          });
+        }
+      }
+    })
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
