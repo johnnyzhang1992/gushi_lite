a8ed31564fd8bf14cf1863267161ce7cf4a93105
johnnyzhang <1113638674@qq.com>
2018-03-11 23:15:57 +0800

    名句页面实现筛选功能

--- pages/poem/sentence/index.js
+++ pages/poem/sentence/index.js
@@ -11,50 +11,9 @@ Page({
     last_page: 1,
     themes: [],
     types:[],
-    multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
-    objectMultiArray: [
-      [
-        {
-          id: 0,
-          name: '无脊柱动物'
-        },
-        {
-          id: 1,
-          name: '脊柱动物'
-        }
-      ], [
-        {
-          id: 0,
-          name: '扁性动物'
-        },
-        {
-          id: 1,
-          name: '线形动物'
-        },
-        {
-          id: 2,
-          name: '环节动物'
-        },
-        {
-          id: 3,
-          name: '软体动物'
-        },
-        {
-          id: 3,
-          name: '节肢动物'
-        }
-      ], [
-        {
-          id: 0,
-          name: '猪肉绦虫'
-        },
-        {
-          id: 1,
-          name: '吸血虫'
-        }
-      ]
-    ],
-    multiIndex: [0, 0, 0]
+    _types:[],
+    th_index:0,
+    ty_index:0
   },
 
   /**
@@ -79,7 +38,8 @@ Page({
             current_page: res.data.poems.current_page,
             last_page: res.data.poems.last_page,
             themes: res.data.themes,
-            types: res.data.types
+            _types: res.data.types,
+            types: res.data.types[0].types
           });
           wx.hideLoading();
         }
@@ -167,73 +127,58 @@ Page({
       }
     }
   },
-  bindMultiPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value);
+  bindPickerThemeChange: function(e) {
+    let that = this;
     this.setData({
-      multiIndex: e.detail.value
+      th_index: e.detail.value,
+      types: that.data._types[e.detail.value].types,
+      ty_index: 0
+    });
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: 'https://xuegushi.cn/wxxcx/getSentenceData?theme='+that.data.themes[e.detail.value],
+      data: {
+        page: 1
+      },
+      success: res =>{
+        if(res.data){
+          console.log('----------success------------');
+          // wx.setStorageSync('user',res.data);
+          // console.log(res.data);
+          that.setData({
+            poems: res.data.poems.data,
+            current_page: res.data.poems.current_page,
+            last_page: res.data.poems.last_page
+          });
+          wx.hideNavigationBarLoading()
+        }
+      }
     })
   },
-  bindMultiPickerColumnChange: function (e) {
-    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
-    var data = {
-      multiArray: this.data.multiArray,
-      multiIndex: this.data.multiIndex
-    };
-    data.multiIndex[e.detail.column] = e.detail.value;
-    switch (e.detail.column) {
-      case 0:
-        switch (data.multiIndex[0]) {
-          case 0:
-            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
-            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
-            break;
-          case 1:
-            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
-            data.multiArray[2] = ['鲫鱼', '带鱼'];
-            break;
+  bindPickerTypeChange: function (e) {
+    let that = this;
+    this.setData({
+      ty_index: e.detail.value
+    });
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: 'https://xuegushi.cn/wxxcx/getSentenceData?theme='+that.data.themes[that.data.th_index]+'&type='+that.data.types[e.detail.value],
+      data: {
+        page: 1
+      },
+      success: res =>{
+        if(res.data){
+          console.log('----------success------------');
+          // wx.setStorageSync('user',res.data);
+          // console.log(res.data);
+          that.setData({
+            poems: res.data.poems.data,
+            current_page: res.data.poems.current_page,
+            last_page: res.data.poems.last_page
+          });
+          wx.hideNavigationBarLoading()
         }
-        data.multiIndex[1] = 0;
-        data.multiIndex[2] = 0;
-        break;
-      case 1:
-        switch (data.multiIndex[0]) {
-          case 0:
-            switch (data.multiIndex[1]) {
-              case 0:
-                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
-                break;
-              case 1:
-                data.multiArray[2] = ['蛔虫'];
-                break;
-              case 2:
-                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
-                break;
-              case 3:
-                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
-                break;
-              case 4:
-                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
-                break;
-            }
-            break;
-          case 1:
-            switch (data.multiIndex[1]) {
-              case 0:
-                data.multiArray[2] = ['鲫鱼', '带鱼'];
-                break;
-              case 1:
-                data.multiArray[2] = ['青蛙', '娃娃鱼'];
-                break;
-              case 2:
-                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
-                break;
-            }
-            break;
-        }
-        data.multiIndex[2] = 0;
-        console.log(data.multiIndex);
-        break;
-    }
-    this.setData(data);
-  },
+      }
+    })
+  }
 });
