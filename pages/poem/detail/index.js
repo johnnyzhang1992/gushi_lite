e1626d7b0ff0ef44b213d285f0e1ebd15b8fceb2
zq19920306 <1113638674@qq.com>
2021-08-06 22:03:46 +0800

    update 移除 canvas 相关

--- pages/poem/detail/index.js
+++ pages/poem/detail/index.js
@@ -1,18 +1,16 @@
 // pages/poem/detail/index.js
-const app = getApp();
 import {
 	GET_POEM_DETAIL,
 	UPDATE_USER_COLLECT,
 	GET_WX_QRCODE,
-	LOADFAIL
+	LOADFAIL,
 } from "../../../apis/request";
-let until = require("../../../utils/util");
+import until from "../../../utils/util";
 const canvas = require("../../../utils/canvas");
 let authLogin = require("../../../utils/authLogin");
-let bg_image = "";
+const app = getApp();
 let poem_detail = {};
-let filePath = "";
-let codePath = "";
+
 Page({
 	/**
 	 * 页面的初始数据
@@ -38,27 +36,27 @@ Page({
 		show_canvas: false,
 		is_ipx: app.globalData.isIpx,
 		dialogShow: false,
-		add_qrcode: false
+		add_qrcode: false,
 	},
 	// 获取用户id
-	getUserId: function() {
+	getUserId: function () {
 		let user = wx.getStorageSync("user");
 		let user_id = user ? user.user_id : 0;
 		this.setData({
-			user_id: user_id
+			user_id: user_id,
 		});
 	},
 	// 返回启动页
-	return: function() {
+	return: function () {
 		wx.switchTab({
-			url: "/pages/index"
+			url: "/pages/index",
 		});
 	},
 	// 滚动切换标签样式
-	switchTab: function(e) {
+	switchTab: function (e) {
 		this.setData(
 			{
-				currentTab: e.detail.current
+				currentTab: e.detail.current,
 			},
 			() => {
 				this.checkCor();
@@ -67,20 +65,20 @@ Page({
 		);
 	},
 	// 点击标题切换当前页时改变样式
-	swichNav: function(e) {
+	swichNav: function (e) {
 		let cur = e.target.dataset.current;
 		// console.log(cur);
 		if (this.data.currentTaB == cur) {
 			return false;
 		} else {
 			this.setData({
-				currentTab: cur
+				currentTab: cur,
 			});
 		}
 		this.changeTabContent(cur);
 	},
 	// 根据tab改变刷新内容
-	changeTabContent: function(cur) {
+	changeTabContent: function (cur) {
 		let that = this;
 		let data = null;
 		if (cur == 3) {
@@ -96,33 +94,33 @@ Page({
 			data = poem_detail.more_infos ? poem_detail.more_infos.content : [];
 		}
 		that.setData({
-			tab_lists: data
+			tab_lists: data,
 		});
 	},
 	//判断当前滚动超过一屏时，设置tab标题滚动条。
-	checkCor: function() {
+	checkCor: function () {
 		if (this.data.currentTab > 4) {
 			this.setData({
-				scrollLeft: 300
+				scrollLeft: 300,
 			});
 		} else {
 			this.setData({
-				scrollLeft: 0
+				scrollLeft: 0,
 			});
 		}
 	},
 	// 跳转到音频页面
-	audio: function() {
+	audio: function () {
 		wx.navigateTo({
 			url:
 				"/pages/poem/audio/index?id=" +
 				this.data.poem.id +
 				"&title=" +
-				this.data.poem.title
+				this.data.poem.title,
 		});
 	},
 	// 复制诗词内容
-	copy: function() {
+	copy: function () {
 		let poem = this.data.poem;
 		let _data =
 			"《" +
@@ -135,21 +133,21 @@ Page({
 			poem.text_content;
 		wx.setClipboardData({
 			data: _data,
-			success: function(res) {
+			success: function (res) {
 				wx.getClipboardData({
-					success: function(res) {
+					success: function (res) {
 						wx.showToast({
 							title: "诗词复制成功",
 							icon: "success",
-							duration: 2000
+							duration: 2000,
 						});
-					}
+					},
 				});
-			}
+			},
 		});
 	},
 	// 渲染tagList
-	renderTagList: function() {
+	renderTagList: function () {
 		let _detail = poem_detail;
 		if (_detail && _detail.yi) {
 			_detail.yi = JSON.parse(_detail.yi);
@@ -164,20 +162,20 @@ Page({
 			_detail.more_infos = JSON.parse(_detail.more_infos);
 		}
 		this.setData({
-			tab_lists: _detail && _detail.zhu ? _detail.zhu.content : null
+			tab_lists: _detail && _detail.zhu ? _detail.zhu.content : null,
 		});
 		poem_detail = _detail;
 	},
 	// 获取诗词详情
-	getPoemDetail: function(poem_id, user_id) {
+	getPoemDetail: function (poem_id, user_id) {
 		let that = this;
 		const data = {
 			id: poem_id,
-			user_id
+			user_id,
 		};
 		//结果以Promise形式返回
 		GET_POEM_DETAIL("GET", data)
-			.then(res => {
+			.then((res) => {
 				if (res.data && res.succeeded) {
 					that.setData({
 						poem: res.data.poem,
@@ -187,12 +185,11 @@ Page({
 								? res.data.poem.tags.split(",")
 								: [],
 						collect_status: res.data.poem.collect_status,
-						is_loading: false
+						is_loading: false,
 					});
 					wx.setNavigationBarTitle({
-						title: res.data.poem.title
+						title: res.data.poem.title,
 					});
-					bg_image = res.data.bg_image;
 					poem_detail = res.data.detail;
 				} else {
 					LOADFAIL();
@@ -201,340 +198,31 @@ Page({
 			.then(() => {
 				that.renderTagList();
 			})
-			.catch(err => {
+			.catch((err) => {
 				console.log(err);
 				LOADFAIL();
 			});
 	},
 	// 点击弹窗取消按钮
-	dialogCancel: function() {
+	dialogCancel: function () {
 		this.setData({
 			dialogShow: false,
-			show_canvas: false
+			show_canvas: false,
 		});
-	},
-	// 添加二维码
-	addQrcode: function() {
-		const { add_qrcode } = this.data;
-		if (add_qrcode) {
-			this.setData(
-				{
-					add_qrcode: false,
-					is_load: false
-				},
-				() => {
-					this.drawImage();
-				}
-			);
-		} else {
-			this.setData(
-				{
-					add_qrcode: true,
-					is_load: false
-				},
-				() => {
-					this.drawImage(true);
-				}
-			);
-		}
-	},
-	// canvas 画图
-	drawImage: function(add_qrcode) {
-		let that = this;
-		const { canvas_img, qr_canvas_img } = that.data;
-		if (add_qrcode && qr_canvas_img) {
-			that.setData({
-				show_canvas: true,
-				is_load: true
-			});
-			wx.hideLoading();
-			return false
-		} else if (!add_qrcode && canvas_img) { 
-			that.setData({
-				show_canvas: true,
-				is_load: true
-			});
-			wx.hideLoading();
-			return false
-		}
-		
-		let content = that.data.content.content;
-		let poemType = that.data.poem.type;
-		let pixelRatio = that.data.pixelRatio;
-		let winWidth = that.data.winWidth;
-		const scale = 1 / pixelRatio;
-		const ctx = wx.createCanvasContext(add_qrcode ? "myCanvas1" : "myCanvas");
-		// 全局设置
-		// ctx.setGlobalAlpha(0.8);
-		let font_size = 16 * pixelRatio;
-		ctx.setFontSize(font_size);
-		// ctx.font = `normal normal normal ${font_size}px/${font_size +
-		// 	18 * pixelRatio}px FangSong, STSong, STZhongsong,"Microsoft YaHei"`;
-		// 计算画布高度
-		let canvasHeight = 140;
-		if (add_qrcode) {
-			canvasHeight += 70;
-		}
-		// 计算标题高度
-		let titleArr = [];
-		titleArr = canvas.breakLinesForCanvas(
-			ctx,
-			that.data.poem.title,
-			winWidth * 0.8 * pixelRatio,
-			18 * pixelRatio
-		);
-		canvasHeight += titleArr.length * 18;
-		// 计算正文
-		let result = [];
-		content.forEach(item => {
-			const itemArr = canvas.breakLinesForCanvas(
-				ctx,
-				poemType === "诗" ? item : "    " + item,
-				winWidth * 0.8 * pixelRatio,
-				16 * pixelRatio
-			);
-			result.push(itemArr);
-		});
-		result.forEach(item => {
-			canvasHeight =
-				canvasHeight + (Array.isArray(item) ? item.length * 28 : 28);
-			if (poemType !== "诗") {
-				canvasHeight += 10;
-			}
-		});
-		console.log(winWidth,canvasHeight)
-		// 计算二维码高度
-		that.setData({
-			canvasHeight: canvasHeight
-		});
-		// 清空画板
-		// ctx.clearRect(0, 0, winWidth * pixelRatio, canvasHeight * pixelRatio);
-		// 画布背景
-		canvas.drawRect(
-			ctx,
-			0,
-			0,
-			winWidth * pixelRatio,
-			canvasHeight * pixelRatio,
-			"#fff"
-		);
-		// 诗词内容
-		let text_y = 30 * pixelRatio;
-		// 标题
-		titleArr.forEach(item => {
-			canvas.drawText(
-				ctx,
-				item,
-				(winWidth * pixelRatio) / 2,
-				text_y * pixelRatio,
-				"center",
-				"#333",
-				(winWidth - 80) * pixelRatio,
-				font_size
-			);
-			text_y += 18;
-		});
-		text_y += 20;
-		// 作者
-		let author = `[${that.data.poem.dynasty}] ${that.data.poem.author}`;
-		canvas.drawText(
-			ctx,
-			author,
-			(winWidth * pixelRatio) / 2,
-			text_y * pixelRatio,
-			"center",
-			"#333",
-			(winWidth - 90) * pixelRatio,
-			font_size
-		);
-		text_y = text_y + 40;
-		// 古诗词正文
-		result.forEach((item,index) => {
-			item.forEach(_item => {
-				canvas.drawText(
-					ctx,
-					_item,
-					poemType === "诗"
-						? (winWidth / 2) * pixelRatio
-						: winWidth * 0.1 * pixelRatio,
-					text_y * pixelRatio,
-					poemType === "诗" ? "center" : "left",
-					"#333",
-					winWidth * 0.8 * pixelRatio,
-					font_size
-				);
-				text_y = text_y + 24;
-			});
-			if (poemType !== "诗" && index<result.length) {
-				text_y += 10;
-			}
-		});
-		// 二维码
-		if (add_qrcode) {
-			font_size = 10 * pixelRatio;
-			ctx.setFontSize(font_size)
-			let _codePath = codePath ? codePath : "/images/xcx1.jpg";
-			let img_width = 50;
-			let img_x = winWidth / 2;
-			let img_y = text_y;
-			canvas.drawCircleImage(
-				ctx,
-				(img_width / 2 + 5) * pixelRatio,
-				img_width * pixelRatio,
-				img_x * pixelRatio,
-				(img_y + img_width / 2 + 5) * pixelRatio,
-				(img_x - img_width / 2) * pixelRatio,
-				(img_y + 5) * pixelRatio,
-				_codePath
-			);
-			text_y += 70;
-			canvas.drawText(
-				ctx,
-				"古诗文小助手",
-				(winWidth / 2) * pixelRatio,
-				text_y * pixelRatio,
-				"center",
-				"#333",
-				winWidth * 0.8 * pixelRatio,
-				font_size
-			);
-		}
-
-		// 缩放
-		ctx.scale(scale, scale);
-		console.log("---huatu" + add_qrcode);
-		// 画图
-		ctx.draw(false, () => {
-			console.log("画图结束，生成临时图...");
-			wx.canvasToTempFilePath({
-				x: 0,
-				y: 0,
-				width: winWidth * pixelRatio,
-				height: canvasHeight * pixelRatio,
-				destWidth: winWidth * pixelRatio,
-				destHeight: canvasHeight * pixelRatio,
-				canvasId: add_qrcode ? "myCanvas1" : "myCanvas",
-				success(res) {
-					console.log(res);
-					if (add_qrcode) {
-						that.setData({
-							show_canvas: true,
-							qr_canvas_img: res.tempFilePath,
-							is_load: true
-						});
-					} else { 
-						that.setData({
-							show_canvas: true,
-							canvas_img: res.tempFilePath,
-							is_load: true
-						});
-					}
-					
-					wx.hideLoading();
-					console.log(res.tempFilePath);
-				}
-			});
-		});
-	},
-	// canvas 生成临时图
-	canvasToImage: function() {
-		let that = this;
-		const { poem } = that.data;
-		// 获取小程序码
-		that.getCodeImage("poem", poem.id);
-		that.setData({
-			show_canvas: true,
-			dialogShow: true
-		});
-		if (!that.data.canvas_img) {
-			wx.showLoading({
-				title: "图片生成中..."
-			});
-			that.drawImage();
-		}
-	},
-	// 保存图片到本地
-	saveImage: function() {
-		let that = this;
-		const { add_qrcode,qr_canvas_img,canvas_img } = that.data;
-		let file_path = add_qrcode ? qr_canvas_img : canvas_img;
-		wx.showLoading({
-			title: "正在保存图片..."
-		});
-		wx.saveImageToPhotosAlbum({
-			filePath: file_path,
-			success(res) {
-				console.log(res);
-				console.log("图片保存完成");
-				wx.hideLoading();
-				wx.showToast({
-					title: "已保存成功",
-					icon: "success",
-					duration: 2000
-				});
-				setTimeout(() => {
-					that.setData({
-						show_canvas: false,
-						dialogShow: false
-					});
-				}, 2000);
-			},
-			fail: res => {
-				console.log(res);
-				wx.hideLoading();
-			}
-		});
-	},
-	// 隐藏canvas画布
-	notSaveImage: function() {
-		this.setData({
-			show_canvas: false
-		});
-	},
-	// 获取小程序码
-	getCodeImage: function(type, id) {
-		let _type = type ? type : "poem";
-		let path = "";
-		if (_type == "poem") {
-			path = "/pages/poem/detail/index?id=" + id;
-		}
-		let data = {
-			type: type,
-			target_id: id,
-			path: path,
-			width: 300
-		};
-		GET_WX_QRCODE("GET", data)
-			.then(res => {
-				if (res.data && res.succeeded) {
-					// 下载小程序码都本地
-					until.downImage(res.data.file_name).then(res1 => {
-						if (res1 && res1.succeeded) {
-							codePath = res1.tempFilePath;
-						}
-					});
-				} else {
-					LOADFAIL();
-				}
-			})
-			.catch(error => {
-				console.log(error);
-				LOADFAIL();
-			});
 	},
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
-	onLoad: function(options) {
+	onLoad: function (options) {
 		let that = this;
 		wx.showLoading({
 			title: "页面加载中...",
-			mask: true
+			mask: true,
 		});
 		this.getUserId();
 		//  高度自适应
 		wx.getSystemInfo({
-			success: function(res) {
+			success: function (res) {
 				let clientHeight = res.windowHeight,
 					clientWidth = res.windowWidth,
 					rpxR = 750 / clientWidth;
@@ -542,16 +230,15 @@ Page({
 				// console.log(calc)
 				that.setData({
 					winHeight1: calc,
-					pixelRatio: res.pixelRatio,
 					winHeight: clientHeight,
-					winWidth: clientWidth
+					winWidth: clientWidth,
 				});
-			}
+			},
 		});
 		that.getPoemDetail(options.id, that.data.user_id);
 	},
 	// 更新收藏情况
-	updateCollect: function() {
+	updateCollect: function () {
 		let that = this;
 		// 判断登陆情况
 		if (that.data.user_id < 1) {
@@ -565,19 +252,19 @@ Page({
 				user_id: that.data.user_id,
 				wx_token: wx.getStorageSync("wx_token"),
 				id: that.data.poem.id,
-				type: "poem"
+				type: "poem",
 			};
 			UPDATE_USER_COLLECT("GET", data)
-				.then(res => {
+				.then((res) => {
 					if (res.data && res.succeeded) {
 						that.setData({
-							collect_status: res.data.status
+							collect_status: res.data.status,
 						});
 					} else {
 						LOADFAIL("更新状态失败，请重试");
 					}
 				})
-				.catch(error => {
+				.catch((error) => {
 					console.log(error);
 					LOADFAIL();
 				});
@@ -586,25 +273,25 @@ Page({
 	/**
 	 * 生命周期函数--监听页面初次渲染完成
 	 */
-	onReady: function() {
+	onReady: function () {
 		wx.hideLoading();
 	},
 	/**
 	 * 用户点击右上角分享
 	 */
-	onShareAppMessage: function() {
+	onShareAppMessage: function () {
 		let that = this;
 		return {
 			title: `${that.data.poem.title} | 古诗文小助手`,
 			path: "/pages/poem/detail/index?id=" + that.data.poem.id,
 			// imageUrl:'/images/poem.png',
-			success: function(res) {
+			success: function (res) {
 				// 转发成功
 				console.log("转发成功！");
 			},
-			fail: function(res) {
+			fail: function (res) {
 				// 转发失败
-			}
+			},
 		};
-	}
+	},
 });
